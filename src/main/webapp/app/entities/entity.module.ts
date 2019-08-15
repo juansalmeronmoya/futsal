093e856570e289cb7fc8943fb7b655fd1b6c07d1
juan.salmeron <jnssterrass@gmail.com>
2019-08-15 22:14:09 +0200

    Revert "Add Team entity"
    This reverts commit e3866406

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,10 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'team',
-        loadChildren: () => import('./team/team.module').then(m => m.FutsalTeamModule)
-      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
